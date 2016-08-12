b47b80483044eaa54543742f8036daf065f42d4e
Ehud <ehudhala@hotmail.com>
2016-08-12 11:53:58 +0300

    Added support for better time representation, and changed the feel of the page.

--- src/donight/web/client/app/app.module.js
+++ src/donight/web/client/app/app.module.js
@@ -7,19 +7,23 @@
             'ngAnimate', 'ngResource', 'ngSanitize',
             // External libraries.
             'ui.bootstrap', 'ui.router', 'ui.select',
+            // Donight dependencies
+            'angularMoment', 'hm.readmore',
             // App modules
             'blocks.router', 'donight.utils', 'donight.events'
         ])
         .run(setGlobalState);
 
     /* @ngInject */
-    function setGlobalState($rootScope, $state, $stateParams) {
+    function setGlobalState($rootScope, $state, $stateParams, amMoment) {
         // It's very handy to add references to $state and $stateParams to the $rootScope
         // so that you can access them from any scope within your applications.For example,
         // <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
         // to active whenever 'contacts.list' or one of its descendants is active.
         $rootScope.$state = $state;
         $rootScope.$stateParams = $stateParams;
+
+        amMoment.changeLocale('he');
     }
 
 })();
