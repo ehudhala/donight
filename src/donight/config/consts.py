09d3af8e88467f403e0bfede91df1152320be9b6
Ehud <ehudhala@hotmail.com>
2016-08-18 20:12:09 +0300

    Added a virtualenv

--- src/donight/config/consts.py
+++ src/donight/config/consts.py
@@ -6,18 +6,18 @@ DEBUG = True
 
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 
-if DEBUG:
-    DB_PATH = os.path.join(ROOT_DIR, 'db.sqlite3')
-    DB_CONNECTION_STRING = 'sqlite:///{0}'.format(DB_PATH)
-else:
+if not DEBUG:
     # Real DB configuration. (Should only be used in production)
-    DB_ENGINE = 'postgresql'
-    DB_ADDRESS = None
-    DB_NAME = None
-    DB_USERNAME = None
-    DB_PASSWORD = None
+    DB_ENGINE = ''
+    DB_ADDRESS = ''
+    DB_NAME = ''
+    DB_USERNAME = ''
+    DB_PASSWORD = ''
     DB_CONNECTION_STRING = '{0}://{1}:{2}@{3}/{4}'.format(
         DB_ENGINE, DB_USERNAME, DB_PASSWORD, DB_ADDRESS, DB_NAME)
+else:
+    DB_PATH = os.path.join(ROOT_DIR, 'db.sqlite3')
+    DB_CONNECTION_STRING = 'sqlite:///{0}'.format(DB_PATH)
 
 LOG_PATH = os.path.join(ROOT_DIR, 'log.txt')
 SCRAPERS_LOG_PATH = os.path.join(ROOT_DIR, 'scrapers_log.txt')
