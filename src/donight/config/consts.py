4c94755980d6646137d54c11233bb71ca722b571
Ehud <ehudhala@hotmail.com>
2016-08-18 16:19:35 +0300

    Allow any engine...

--- src/donight/config/consts.py
+++ src/donight/config/consts.py
@@ -10,13 +10,14 @@ if DEBUG:
     DB_PATH = os.path.join(ROOT_DIR, 'db.sqlite3')
     DB_CONNECTION_STRING = 'sqlite:///{0}'.format(DB_PATH)
 else:
-    # PostgreSQL configuration. (Should only be used in production)
+    # Real DB configuration. (Should only be used in production)
+    DB_ENGINE = 'postgresql'
     DB_ADDRESS = None
     DB_NAME = None
     DB_USERNAME = None
     DB_PASSWORD = None
-    DB_CONNECTION_STRING = 'postgresql://{0}:{1}@{2}/{3}'.format(
-        DB_USERNAME, DB_PASSWORD, DB_ADDRESS, DB_NAME)
+    DB_CONNECTION_STRING = '{0}://{1}:{2}@{3}/{4}'.format(
+        DB_ENGINE, DB_USERNAME, DB_PASSWORD, DB_ADDRESS, DB_NAME)
 
 LOG_PATH = os.path.join(ROOT_DIR, 'log.txt')
 SCRAPERS_LOG_PATH = os.path.join(ROOT_DIR, 'scrapers_log.txt')
