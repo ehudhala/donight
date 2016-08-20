2505bf26621ec1809d5dde9f1249bb0f136bd4ee
Ehud <ehudhala@hotmail.com>
2016-08-20 15:49:16 +0300

    Don't log everything.

--- src/donight/config/logging.py
+++ src/donight/config/logging.py
@@ -55,9 +55,9 @@ LOGGING_CONFIG = {
             'level': 'DEBUG',
             'propagate': False
         },
-        '': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-        },
+        # '': {
+        #     'handlers': ['externals_file'],
+        #     'level': 'DEBUG',
+        # },
     }
 }
