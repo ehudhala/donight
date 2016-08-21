88095a0a51df5dcf988ec0cec15086dd523f50f0
Ehud <ehudhala@hotmail.com>
2016-08-22 01:10:21 +0300

    Small fix with the logger name.

--- src/donight/scripts/index.py
+++ src/donight/scripts/index.py
@@ -4,6 +4,6 @@ from donight.config.consts import DEBUG
 from donight.event_finder.core import EventFinder
 
 if __name__ == '__main__':
-    logger = getLogger(__name__)
+    logger = getLogger('donight.scripts.index')
     logger.info("Mode: {0}".format('Debug' if DEBUG else 'Production'))
     EventFinder().index_events()
